389c5f35bb7cf0b434298e12afaffd779d9dacd8
Jacopo Mangiavacchi <jamangia@microsoft.com>
2020-05-11 16:01:24 -0700

    skeleton

---
+++ TestAppCenter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestAppCenter
+//
+//  Created by Jacopo Mangiavacchi on 5/11/20.
+//  Copyright © 2020 Jacopo Mangiavacchi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
